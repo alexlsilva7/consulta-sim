950928455601fe479bb9516e388a83c2800facdd
Alex Lopes <alex.lsilva154@gmail.com>
2022-08-13 06:56:18 -0300

    adicionado validações em endereco

--- app/models/endereco.rb
+++ app/models/endereco.rb
@@ -1,3 +1,15 @@
 class Endereco < ApplicationRecord
   belongs_to :paciente
+
+  validates :cidade, :bairro, :logradouro, :complemento, :cep, presence: true
+
+  validates :cep, length: {is:8}
+  validates :cep, numericality: { only_integer: true }
+
+  validates :cidade, :bairro, :logradouro, :complemento, length: { minimum: 4}
+  validates :cidade, :bairro, :logradouro, :complemento, length: { maximum: 30}
+
+
+
+
 end
