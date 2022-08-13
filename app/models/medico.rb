cb6a243ede127fad3cd2a3e08b2f51b1e7d37e9c
Alex Lopes <alex.lsilva154@gmail.com>
2022-08-13 07:18:10 -0300

    adicionado validações em medico

--- app/models/medico.rb
+++ app/models/medico.rb
@@ -1,3 +1,17 @@
 class Medico < ApplicationRecord
-    has_many :consultums,dependent: :destroy
+    has_many :consultums, dependent: :destroy
+
+    validates :cpf , :email , :nome_completo, :especialidade, :crm, presence: true
+    validates :cpf, numericality: { only_integer: true }
+    validates :cpf, length: { is: 11}
+    validates :crm, length: { is: 8}
+
+    validates :nome_completo, :especialidade, length: { minimum: 5}
+    validates :nome_completo, :especialidade, length: { maximum: 30}
+
+
+
+    validates :email, :crm, uniqueness: true
+    validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+
 end
