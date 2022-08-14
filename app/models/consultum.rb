20a7b5ad003dc177983aadf549f7c408f28ea29f
Alex Lopes <alex.lsilva154@gmail.com>
2022-08-14 13:19:59 -0300

    validação de consultas

--- app/models/consultum.rb
+++ app/models/consultum.rb
@@ -4,4 +4,22 @@ class Consultum < ApplicationRecord
 
   validates :data , :horario, :medico_id, :paciente_id, presence: true
 
+  validate :data_future
+  validate :date_is_not_sunday
+
+  validates_time :horario, :between => '9:00am'..'5:00pm'
+
+  def data_future
+    if !data.nil?
+      if  data < Date.today
+        errors.add(:data, "can't be in the past")
+      end
+    end
+  end
+
+  def date_is_not_sunday
+    if data.present? && data.wday == 0
+      errors.add(:date, "Date can't be sunday")
+    end
+  end
 end
